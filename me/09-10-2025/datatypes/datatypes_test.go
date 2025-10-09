3f9e418b8a744f823764ca44a52423df976dbb40
Lucas <123@gmx.de>
2025-10-09 17:25:38 +0200

    .

---
+++ me/09-10-2025/datatypes/datatypes_test.go
@@ -0,0 +1,37 @@
+package datatypes
+
+import "fmt"
+
+func Example_integers() {
+	var i int
+	k := 55
+
+	fmt.Println(i)
+	fmt.Println(k)
+
+	// Output:
+	// 0
+	// 55
+}
+
+func Example_various_data_types() {
+	var i1 int
+	i2 := 55
+
+	fmt.Println(i1)
+	fmt.Println(i2)
+
+	var f1 float64
+	f2 := 3.0 / 4.0
+
+	fmt.Println(f1)
+	fmt.Println(f2)
+
+	var b1 bool
+	b2 := true
+
+	fmt.Println(b1)
+	fmt.Println(b2)
+
+	// Output:
+}
