3bdec5658dc304c7604f17f9e87f58584ab18910
Lucas <123@gmx.de>
2025-10-16 14:43:23 +0200

    fizz beispiel

---
+++ me/16-10-2025/fizzbuzz/fizz.go
@@ -0,0 +1,28 @@
+package fizzbuzz
+
+import "fmt"
+
+// Fizz gibt die Zahlen von 1 bis 30 aus und ersetz dabei jede
+// durch 3 teilbare Zahl durch "fizz" und jede durch 5 teilbare
+// Zahl durch "buzz". Bei Zahlen, die sowohl durch 3 als auch durch
+// 5 teilbar sind, wird "fizzbuzz" ausgegeben.
+func Fizz() {
+	for i := 1; i <= 30; i++ {
+		// wenn i durch 3 und 5 teilbar ist, gib "fizzbuzz" aus
+		if i%3 == 0 && i%5 == 0 {
+
+			fmt.Println("fizzbuzz")
+		} else if
+		// wenn i durch 3 teilbar ist, gib "fizz" aus
+		i%3 == 0 {
+			fmt.Println("fizz")
+		} else if
+		// wenn i durch 5 teilbar ist, gib "buzz" aus
+		i%5 == 0 {
+			fmt.Println("buzz")
+		} else {
+			// sonst gib i aus
+			fmt.Println(i)
+		}
+	}
+}
